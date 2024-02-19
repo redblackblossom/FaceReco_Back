3b7af3105d47c805595492c47d1957c3427ed490
redblackblossom <155342235+redblackblossom@users.noreply.github.com>
2024-02-19 21:49:33 +0900

    first commit

---
+++ src/main/java/image/faceReco/FaceRecoApplication.java
@@ -0,0 +1,13 @@
+package image.faceReco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceRecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/image/faceReco/FaceRecoApplicationTests.java
@@ -0,0 +1,13 @@
+package image.faceReco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceRecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
